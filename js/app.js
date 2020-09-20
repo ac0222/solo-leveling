4080e55903e9e51c3378d9dbb175a0794ac238b0
Shi Chang Zhang <changz@live.com.au>
2020-09-21 01:43:55 +1000

    fix hp bug

--- js/app.js
+++ js/app.js
@@ -1,13 +1,50 @@
+let playerMoves = [
+    {
+        name: 'bash',
+        damage: 5,
+        block: 0,
+        heal: 0,
+        buffAtk: 0,
+        buffDef: 0,
+    },
+    {
+        name: 'barrier',
+        damage: 0,
+        block: 10,
+        heal: 0,
+        buffAtk: 0,
+        buffDef: 0,
+    },
+    {
+        name: 'do some push ups',
+        damage: 0,
+        block: 2,
+        heal: 0,
+        buffAtk: 2,
+        buffDef: 2,
+    },
+    {
+        name: 'inject roids',
+        damage: 0,
+        block: 0,
+        heal: -5,
+        buffAtk: 8,
+        buffDef: -1,
+    },
+];
+
 let player = {
     level: 0,
     currentExp: 0,
     maxExp: 10,
-    currentHp: 50,
-    maxHp: 50,
+    hp: 50,
     atk: 5,
     def: 5,
     block: 0,
-    imageUrl: 'assets/sungjinwoo.png'
+    imageUrl: 'assets/sungjinwoo.png',
+    nextMove: null,
+    moves: playerMoves,
+    warningMessage: '' 
 };
 
 let goblin = {
@@ -63,41 +100,6 @@ let opponents = [
 
 let currentOpponent = { current: null };
 
-let playerMoves = [
-    {
-        name: 'bash',
-        damage: 5,
-        block: 0,
-        heal: 0,
-        buffAtk: 0,
-        buffDef: 0,
-    },
-    {
-        name: 'barrier',
-        damage: 0,
-        block: 10,
-        heal: 0,
-        buffAtk: 0,
-        buffDef: 0,
-    },
-    {
-        name: 'do some push ups',
-        damage: 0,
-        block: 2,
-        heal: 0,
-        buffAtk: 2,
-        buffDef: 2,
-    },
-    {
-        name: 'inject roids',
-        damage: 0,
-        block: 0,
-        heal: -5,
-        buffAtk: 8,
-        buffDef: -1,
-    },
-];
-
 let opponentMoves = [
     {
         name: 'claw',
@@ -142,29 +144,29 @@ function getRandomInt(min, max) {
 
 function runTurn(game) {
     // player takes their turn
-    applyMove(game.player, game.ant, playerMoves[game.nextPlayerMove]);
-    console.log(game)
+    applyMove(game.player, game.opponent, game.player.nextMove);
+    // console.log(game);
     let res = checkWin(game);
     if (res !== 'continue') {
         return res;
     }
 
     // enemy takes their turn
-    applyMove(game.ant, game.player, antMoves[game.nextAntMove]);
-    console.log(game)
+    applyMove(game.opponent, game.player, game.opponent.nextMove);
+    // console.log(game);
     return checkWin(game);
 }
 
 function runGame(game) {
     let i = 0;
-    console.log('turn: ', i);
+    // console.log('turn: ', i);
     let res = runTurn(game);
     prepareNextTurn(game);
     i += 1;
 }
 
 function applyMove(actor, target, move) {
-    actor.hitpoints += move.heal;
+    actor.hp += move.heal;
     if (move.block > 0) {
         actor.block += (move.block + actor.def);
     }
@@ -174,7 +176,7 @@ function applyMove(actor, target, move) {
     if (move.damage > 0) {
         let damageDealt = (move.damage + actor.atk) - (target.block);
         if (damageDealt > 0) {
-            target.hitpoints -= damageDealt;
+            target.hp -= damageDealt > target.hp ? target.hp : damageDealt;
         }
     }
     // block is removed at the end of the turn next turn
@@ -182,38 +184,13 @@ function applyMove(actor, target, move) {
 }
 
 function checkWin(game) {
-    if (game.player.hitpoints <= 0) {
+    if (game.player.hp <= 0) {
         return 'lose';
     }
-    if (game.ant.hitpoints <= 0) {
+    if (game.opponent.hp <= 0) {
         return 'win';
     }
     return 'continue';
-}
-
-function updatePlayerMoveDetails(game) {
-    document.createElement();
-}
-
-function createMoveDetails(actor, move) {
-    let moveDetails = document.createElement('div');
-    let attackFor = move.damage > 0 ? actor.atk + move.damage : 0;
-    let blockFor = move.block > 0 ? actor.def + move.block : 0;
-    moveDetails.appendChild(document.createElement('div')).textContent = 'Attack for: ' + attackFor;
-    moveDetails.appendChild(document.createElement('div')).textContent = 'Block for: ' + blockFor;
-    moveDetails.appendChild(document.createElement('div')).textContent = 'heal for: ' +  move.heal;
-    moveDetails.appendChild(document.createElement('div')).textContent = 'Buff defense by: ' + move.buffDef;
-    moveDetails.appendChild(document.createElement('div')).textContent = 'Buff attack by: ' + move.buffAtk;
-    return moveDetails;
-}
-
-function updateUI(game) {
-    document.getElementById('player-hp').textContent = game.player.hitpoints;
-    document.getElementById('ant-hp').textContent = game.ant.hitpoints;
-    document.getElementById('ant-current-block').textContent = game.ant.block;
-    document.getElementById('next-ant-move').textContent = antMoves[game.nextAntMove].name;
-    document.getElementById('ant-move-details').innerHTML = '';
-    document.getElementById('ant-move-details').appendChild(createMoveDetails(game.ant, antMoves[game.nextAntMove]));
 }
 
 function getNextOpponentMove() {
@@ -223,44 +200,21 @@ function getNextOpponentMove() {
 
 function prepareNextTurn(game) {
     game.opponent.nextMove = getNextOpponentMove();
-    updateUI(game);
 }
 
 function init(game) {
-    
-    let i = 0;
-    for (move of playerMoves) {
-        let moveButton = document.createElement('button');
-        moveButton.addEventListener('click', function(e) {
-            console.log(e.target.getAttribute('index'));
-            game.nextPlayerMove = e.target.getAttribute('index');
-            document.getElementById('player-move-details').innerHTML = '';
-            document.getElementById('player-move-details').appendChild(createMoveDetails(game.player, playerMoves[game.nextPlayerMove]));
-        });
-        moveButton.textContent = move.name;
-        moveButton.setAttribute('index', i);
-        document.getElementById('player-moves').appendChild(moveButton);
-        i += 1;
-    }
-
-    document.getElementById('end-turn').addEventListener('click', function() {
-        if (game.nextPlayerMove === -1) {
-            document.getElementById('warning-message').textContent = 'Pick a move first you noob';
-        } else {
-            document.getElementById('warning-message').textContent = '';
-            runGame(game);
-        }
-    });
     prepareNextTurn(game);
 }
 
 function startBattleWith (opponent) {
-    currentOpponent.current = { ...opponent, nextMove: getNextOpponentMove(), moves: opponentMoves };
+    currentOpponent.current = { ...opponent, nextMove: null, moves: opponentMoves };
+    game.opponent = currentOpponent.current;
+    init(game);
 }
 
 Vue.component('health-bar', {
     props: ['player'],
-    template: '<ul><li>You</li><li>Level: {{ player.level }}</li><li>HP: {{ player.currentHp }}/{{ player.maxHp }}</li><li>EXP: {{ player.currentExp }}/{{ player.maxExp }}</li><li><img v-bind:src="player.imageUrl"></li></ul>'
+    template: '<div><h2>You</h2><div><img v-bind:src="player.imageUrl"></div><div>Level: {{ player.level }}</div><div>HP: {{ player.hp }}</div><div>EXP: {{ player.currentExp }}/{{ player.maxExp }}</div></div>'
 });
 
 Vue.component('opponent-summary', {
@@ -294,8 +248,9 @@ Vue.component('player-moves', {
             </div>
             <h2>Move details</h2>
             <move-details v-if='player.nextMove !== null' v-bind:player='player'></move-details>
-            <button id='end-turn'>Go</button>
-            <div id='warning-message'></div>
+            <button v-if='player.nextMove === null' v-on:click='player.warningMessage="Pick a move first you noob";'>Go</button>
+            <button v-else v-on:click='player.warningMessage=""; runGame(game);'>Go</button>
+            <div>{{ player.warningMessage }}</div>
         </div>
 `
 });
@@ -308,7 +263,7 @@ Vue.component('opponent', {
             <img v-bind:src='opponent.imageUrl'>
             <div>HP: {{ opponent.hp }}</div>
             <div>Current block: {{ opponent.block }}</span></div>
-            <div>Next move: {{ opponent.nextMove.name }}</div>
+            <div>Next move: {{ opponent.nextMove !== null ? opponent.nextMove.name : '' }}</div>
             <h2>Move details</h2>
             <move-details v-if='opponent.nextMove !== null' v-bind:player='opponent'></move-details>
             </div>
@@ -319,7 +274,7 @@ Vue.component('opponent', {
 var scene = new Vue({
     el: '#scene',
     data: {
-        player: { ...player, nextMove: null,  moves: playerMoves },
+        player: player,
         opponent: currentOpponent,
         opponents: opponents,
         game: game
